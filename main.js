75ddbb8f6983fe72884ec1ca3b38db28b9372884
cad6764418097000 <74503191+cad6764418097000@users.noreply.github.com>
2022-09-30 11:57:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 
 
 
-
-class Ellipse{
+class Ellipse extends Object{
   constructor(x, y, z, r, verticeDensity){ // verticieDensity defined the number of points along each circle and how manny circles there are
     // x^2 + y^2 = 20
     // +/-√20 - x^2 = y
@@ -13,10 +11,14 @@ class Ellipse{
     let vertices = [];
     let edges = [];
     let angleSeperation = (360/verticeDensity); // calculates how much each point is to be seperated to be equally distributed
-    console.log(x+","+( y + r));
+    super(x, y, z);
+
+    // Establishes the main circle for rotation
     for (var i = 0; i < verticeDensity; i++) {
       var rot = angleSeperation * i;
-      vertices.push(rotatePoint2dSpace(x, y, x, y + r, rot));
+      var point = rotatePoint2dSpace(x, y, x, y - r, rot);
+      point.push(0); // z value
+      vertices.push(point);
 
       if (i === verticeDensity - 1) {
         edges.push([0, i])
@@ -25,149 +27,88 @@ class Ellipse{
       }
     }
 
+    // Estblished a second circle on the z axis for each
+    var dimCircle = [];
+    for (var j = 0; j < verticeDensity; j++) {
+      // z is the new x and x is the new y in this dimension
+      // take one half of the circle and rotate it
+      if (j < verticeDensity / 2) {
+        var opIndex = verticeDensity - j;
+        if(opIndex % verticeDensity >= 0){  // Reset opIndex to the overflow if it's greater than the number of verticies
+          opIndex = opIndex % verticeDensity;
+        }
+
+        for (var i = 0; i < verticeDensity; i++) {
+          var rot = angleSeperation * i;
+          var radius = (vertices[j][0] - vertices[opIndex][0]) / 2;
+
+          var y = vertices[j][1];
+
+
+          let center = [z, x];
+          //console.log(radius);
+          let rotPoint = [vertices[j][2], vertices[0][0] + radius]
+          //console.log(j+ ", " + opIndex + ":   rotNumber: " + i + ", rot: " + rot + ", ||" + radius);
+          //console.log("center : ("+center[0] + " , " +center[1] + ") , rotpoint: ("  + rotPoint[0] + " , "  +(rotPoint[1]) + ")");
+
+          var point = rotatePoint2dSpace(center[0], center[1], rotPoint[0], rotPoint[1], rot);
+          //console.log(point);
+          var dPoint = [point[1], y, point[0]]; // x, y, z
+          //console.log(dPoint);
+          vertices.push(dPoint);
+
+          // Appends the edges for each circle   i === verticeDensity - 1
+          let currentIndex = j * (verticeDensity) + i;
+          if (currentIndex === verticeDensity * (j + 1) - 1) {
+            edges.push([j * (verticeDensity), currentIndex])
+          }else{
+            edges.push([currentIndex, currentIndex + 1]);
+          }
+        }
+
+
+
+      }
+    }
+
     this.vertices = vertices;
     this.edges = edges;
   }
 
 
-
-  draw(vertices = false){
-    // Draws lines in between the vertices
-    for(let j = 0; j < this.edges.length; j++){
-      let vertex1 = this.vertices[this.edges[j][0]];
-      let vertex2 = this.vertices[this.edges[j][1]];
-      ctx.beginPath();
-      ctx.moveTo(vertex1[0], vertex1[1]);
-      ctx.lineTo(vertex2[0], vertex2[1]);
-      ctx.stroke();
-    }
-
-
-    if (vertices) {
-      // draw vertices
-      for (var i = 0; i < this.vertices.length; i++) {
-        let vertex = this.vertices[i];
-        ctx.beginPath();
-        ctx.fillStyle = "blue";
-        ctx.arc(vertex[0], vertex[1], 3, 0, 2 * Math.PI);
-        ctx.fill();
-      }
-
-
-    }
-
 }
 
-}
-class Cuboid{
+
+
+
+
+
+
+
+class Cuboid extends Object{
 // some things taken from https://www.khanacademy.org/computing/computer-programming/programming-games-visualizations/programming-3d-shapes/a/generating-3d-shapes
   constructor(x, y, z, w, h, d){
+      super(x + w / 2, y + h / 2, z + d / 2);
       this.vertices = [[x, y, z ], [x, y, z+d], [x, y+h, z ], [x, y+h, z+d], [x+w, y, z ], [x+w, y, z+d], [x+w, y+h, z ], [x+w, y+h, z+d]];
       // defines the 2 verticies that make up an edge using the index vaule
       this.edges = [[0, 1], [1, 3], [3, 2], [2, 0], [4, 5], [5, 7], [7, 6], [6, 4], [0, 4], [1, 5], [2, 6], [3, 7]];
-      this.rotation = [0,0,0];
-      // Set the rotation center to the objects center
-      this.rotationCenter = [x + w/2, y + h/2, z + d/2];
+
+
    }
-
-
-
-  draw(vertices = false){
-    // Draws lines in between the vertices
-    for(let j = 0; j < this.edges.length; j++){
-      let vertex1 = this.vertices[this.edges[j][0]];
-      let vertex2 = this.vertices[this.edges[j][1]];
-      ctx.beginPath();
-      ctx.moveTo(vertex1[0], vertex1[1]);
-      ctx.lineTo(vertex2[0], vertex2[1]);
-      ctx.stroke();
-    }
-
-    if (vertices) {
-      // draw vertices
-      for (var i = 0; i < this.vertices.length; i++) {
-        let vertex = this.vertices[i];
-        ctx.beginPath();
-        ctx.fillStyle = "blue";
-        ctx.arc(vertex[0], vertex[1], 3, 0, 2 * Math.PI);
-        ctx.fill();
-      }
-
-
-    }
-  }
-    rotate(axis, theta){
-      var sin = Math.sin(convertAngle("radians", theta));
-      var cos = Math.cos(convertAngle("radians", theta));
-
-      // Rotation coordinates
-      let cx = this.rotationCenter[0];
-      let cy = this.rotationCenter[1];
-      let cz = this.rotationCenter[2];
-
-
-      for (var i = 0; i < this.vertices.length; i++) {
-        let vertex = this.vertices[i];
-        // Realations between center for formula of rotation around a point later
-        /*
-        x1 = (x0 – xc)cos(θ) – (y0 – yc)sin(θ) + xc
-        y1 = (x0 – xc)sin(θ) + (y0 – yc)cos(θ) + yc
-        */
-        var x = vertex[0] - cx;
-        var y = vertex[1] - cy;
-        var z = vertex[2] - cz;
-
-
-        //console.log(vertex);
-
-        if (axis === "x") {
-          vertex[1] = y * cos - z * sin + cy;
-          vertex[2] = z * cos + y * sin + cz;
-
-          this.rotation[0] += theta / this.vertices.length; // Counter the loop
-        }else if(axis === "y"){
-          vertex[0] = x * cos + z * sin + cx;
-          vertex[2] = z * cos - x * sin + cz;
-          this.rotation[1] += theta / this.vertices.length;
-        }else if(axis === "z"){
-          vertex[0] = x * cos - y * sin + cx;
-          vertex[1] = y * cos + x * sin + cy;
-          this.rotation[2] += theta / this.vertices.length;
-        }
-        //console.log(vertex);
-        this.vertices[i] = vertex;
-      }
-
-    }
-
-/*
-  ctx.beginPath();
-  ctx.moveTo(leftX, topY);
-  ctx.lineTo(rightX, topY);
-  ctx.moveTo(leftX, topY);
-  ctx.lineTo(leftX, bottomY);
-
-  ctx.stroke();
-
-
-
-  ctx.beginPath();
-  ctx.rect(leftX, topY, size, size);
-  ctx.stroke();
-
-  drawSide(200, 200, 20);
-  */
 }
 
+
+
 //x, y, z, r, verticeDensity
-var sphere = new Ellipse(100,100,100, 1,30);
+var sphere = new Ellipse(200,200,0, 100,20);
 console.log(sphere);
 sphere.draw(true);
 
 
 
 
-var box = new Cuboid(230,230,230,100,100,100);
+
+//var box = new Cuboid(230,230,230,100,100,100);
 
 
 //box.draw(true);
@@ -189,10 +130,10 @@ document.getElementById("z").oninput = function() {
 
 function handleSliders(axis, number){
   let value = document.getElementById(axis).value;
-  let theta = value - box.rotation[number];
-  box.rotate(axis, theta);
+  let theta = value - sphere.rotation[number];
+  sphere.rotate(axis, theta);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  box.draw(true);
+  sphere.draw(true);
 
 }
 
@@ -206,8 +147,8 @@ function convertAngle(returnType, input){
 function rotatePoint2dSpace(cx, cy, x, y, degrees){
   let cos = Math.cos(convertAngle("radians", degrees));
   let sin = Math.sin(convertAngle("radians", degrees));
-  let point = [0, 0, 1];
-  point[0] = x * cos - y * sin + cx;
-  point[1] = y * cos + x * sin + cy;
+  let point = [0, 0];
+  point[0] = (x - cx) * cos - (y - cy) * sin + cx;
+  point[1] = (y - cy) * cos + (x - cx) * sin + cy;
   return point;
 }
