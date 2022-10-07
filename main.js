c0fc68e8c4a7cf4e9a81783461e7e8214366c374
cad6764418097000 <74503191+cad6764418097000@users.noreply.github.com>
2022-10-07 12:43:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
-
+var objects = [];
 
 
 class Ellipse extends Object{
@@ -78,12 +78,16 @@ class Ellipse extends Object{
     }
 
 
-
-
-
+    for (var i = 0; i < vertices.length; i++) {
+      vertices[i].push(false); // keeps track of verticie selection status
+    }
 
     this.vertices = vertices;
     this.edges = edges;
+
+
+
+
   }
 
 
@@ -100,30 +104,29 @@ class Cuboid extends Object{
 // some things taken from https://www.khanacademy.org/computing/computer-programming/programming-games-visualizations/programming-3d-shapes/a/generating-3d-shapes
   constructor(x, y, z, w, h, d){
       super(x + w / 2, y + h / 2, z + d / 2);
-      this.vertices = [[x, y, z ], [x, y, z+d], [x, y+h, z ], [x, y+h, z+d], [x+w, y, z ], [x+w, y, z+d], [x+w, y+h, z ], [x+w, y+h, z+d]];
+      this.vertices = [[x, y, z, false ], [x, y, z+d, false], [x, y+h, z, false], [x, y+h, z+d, false], [x+w, y, z, false], [x+w, y, z+d, false], [x+w, y+h, z, false], [x+w, y+h, z+d, false]];
       // defines the 2 verticies that make up an edge using the index vaule
-      this.edges = [[0, 1], [1, 3], [3, 2], [2, 0], [4, 5], [5, 7], [7, 6], [6, 4], [0, 4], [1, 5], [2, 6], [3, 7]];
-
-
+      this.edges = [[0, 1], [1, 3], [3, 2], [2, 0], [4, 5], [5, 7], [7, 6], [6, 4], [0, 4], [1, 5], [2, 6], [3, 7]]; // edges are defined with 2 verticies
+      this.faces = [[0,1,2,3], [4,5,6,7], [9, 0, 8, 0], [11,0,10,0], [9, 0, 11, 0], [8, 0, 10,0]]; // faces are defined with 4 edges
    }
 }
 
 
 
 //x, y, z, r, verticeDensity
-var sphere = new Ellipse(200,200,0, 200,10);
+var sphere = new Ellipse(200,200,0, 150,20);
 console.log(sphere);
-sphere.draw(true);
+sphere.draw("frame");
+objects.push(sphere);
 
 
 
 
+var box = new Cuboid(230,230,0,100,100,100);
+box.rotationCenter = sphere.rotationCenter;
 
-//var box = new Cuboid(230,230,230,100,100,100);
-
-
-//box.draw(true);
-
+box.draw("frame");
+objects.push(box);
 
 
 
@@ -143,8 +146,15 @@ function handleSliders(axis, number){
   let value = document.getElementById(axis).value;
   let theta = value - sphere.rotation[number];
   sphere.rotate(axis, theta);
+
+  let theta2 = value - box.rotation[number];
+  box.rotate(axis, theta2);
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  sphere.draw(true);
+  //sphere.draw("solid");
+  box.draw("solid");
+  box.draw("frame");
+
 
 }
 
@@ -162,4 +172,25 @@ function rotatePoint2dSpace(cx, cy, x, y, degrees){
   point[0] = (x - cx) * cos - (y - cy) * sin + cx;
   point[1] = (y - cy) * cos + (x - cx) * sin + cy;
   return point;
+}
+function drawQuadrilateral(points, fillStyle){ // where points is an array of 4 points
+
+  let point1 = points[0];
+  let point2 = points[1];
+  let point3 = points[2];
+  let point4 = points[3];
+
+  ctx.beginPath();
+  ctx.moveTo(point1[0], point1[1]);
+  ctx.lineTo(point2[0], point2[1]);
+  ctx.lineTo(point3[0], point3[1]);
+  ctx.lineTo(point4[0], point4[1]);
+  ctx.lineTo(point1[0], point1[1]);
+  ctx.fillStyle = fillStyle;
+  ctx.fill();
+}
+
+function sortPoints(a, b){
+
+
 }
